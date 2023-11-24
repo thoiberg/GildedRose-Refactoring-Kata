acd75cd830dc33739e8b702aced12a1025810dc1
Tim Hoiberg <thoiberg@degica.com>
2023-11-24 16:59:46 +0900

    Remove old update method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -14,54 +14,6 @@ class GildedRose
 
     @items = new_items
   end
-
-  def old_update_quality
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
-  end
 end
 
 class Item
@@ -147,11 +99,11 @@ class BackstagePassPricingRule < PricingRule
 end
 
 class PricingEngine
-  DEFAULT_PRICING_RULE = ::GeneralItemPricingRule
+  DEFAULT_PRICING_RULE = GeneralItemPricingRule
   PRICING_RULES = {
-    "Aged Brie" => ::BriePricingRule,
-    "Sulfuras, Hand of Ragnaros" => ::SulfurasPricingRule,
-    "Backstage passes to a TAFKAL80ETC concert" => ::BackstagePassPricingRule
+    "Aged Brie" => BriePricingRule,
+    "Sulfuras, Hand of Ragnaros" => SulfurasPricingRule,
+    "Backstage passes to a TAFKAL80ETC concert" => BackstagePassPricingRule
   }
 
   def apply(item:)
